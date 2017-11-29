b38c7cac8caf7090c245f5c20006167da976bcd9
minieyes <chenvey2@gmail.com>
2017-11-29 22:32:15 +0800

    autorun add no-ip

--- autorun/autorun.py
+++ autorun/autorun.py
@@ -3,3 +3,6 @@ import os
 
 os.chdir(r'C:\Program Files\Python36\minieyes\autorun')
 exec(open('LoanNotify.py').read(), globals())
+
+os.chdir(r"C:\Program Files (x86)\No-IP")
+os.system("DUC40.exe")

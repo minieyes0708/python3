0a205c44229cb8467c5abca7bf64518939e72687
minieyes <chenvey2@gmail.com>
2017-11-30 22:58:44 +0800

    system NoIp use START to avoid blocking

--- autorun/autorun.py
+++ autorun/autorun.py
@@ -5,4 +5,4 @@ os.chdir(r'C:\Program Files\Python36\minieyes\autorun')
 exec(open('LoanNotify.py').read(), globals())
 
 os.chdir(r"C:\Program Files (x86)\No-IP")
-os.system("DUC40.exe")
+os.system('START "" DUC40.exe')

3c6de84e967a643c82fc290c0b8c77c46b02e37d
minieyes <chenvey2@gmail.com>
2018-09-07 21:15:52 +0800

    add click weekly line

--- read_reserved.py
+++ read_reserved.py
@@ -13,6 +13,11 @@ while command != 'q' and len(reserved) != 0:
     stock_id = reserved[0]
     web.get('http://www.cmoney.tw/finance/f00025.aspx?s=' + str(stock_id))
     web.execute_script("window.scrollTo(0,250);");
+    frames = web.find_elements_by_tag_name('iframe')
+    web.switch_to_frame(frames[0])
+    btns = web.find_elements_by_tag_name('input')
+    btns[1].click()
+    web.switch_to_default_content()
     del reserved[0]
     command = input('(q to quit) (%d)(%d) >> ' % (len(reserved), stock_id))
 
--- read_stocks.py
+++ read_stocks.py
@@ -15,6 +15,11 @@ while command != 'q' and len(stocks) != 0:
     stock_id = stocks[0]
     web.get('http://www.cmoney.tw/finance/f00025.aspx?s=' + str(stock_id))
     web.execute_script("window.scrollTo(0,250);");
+    frames = web.find_elements_by_tag_name('iframe')
+    web.switch_to_frame(frames[0])
+    btns = web.find_elements_by_tag_name('input')
+    btns[1].click()
+    web.switch_to_default_content()
     del stocks[0]
     command = input('(q to quit) (%d)(%d) >> ' % (len(stocks), stock_id))
 

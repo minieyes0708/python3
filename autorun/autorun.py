014f6fc94cecd7076698d3b49cee50ab2056afda
minieyes <chenvey2@gmail.com>
2018-09-15 18:45:50 +0800

    no more car loans

--- autorun/autorun.py
+++ autorun/autorun.py
@@ -1,8 +1,8 @@
 # -*- coding=cp950 -*-
 import os
 
-os.chdir(r'C:\Program Files\Python36\minieyes\autorun')
-exec(open('LoanNotify.py').read(), globals())
+# os.chdir(r'C:\Program Files\Python36\minieyes\autorun')
+# exec(open('LoanNotify.py').read(), globals())
 
 os.chdir(r"C:\Program Files (x86)\No-IP")
 os.system('START "" DUC40.exe')

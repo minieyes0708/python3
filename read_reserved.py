7057b9638c20a822652ce435be9f605dd765a7f4
minieyes <chenvey2@gmail.com>
2019-05-28 21:11:30 +0800

    Don't use ©P½u
    - optionally show 3034 first in case some dialog popup
      so user have the oppertunity to close it
    - retain stock.txt contents after executed

--- read_reserved.py
+++ read_reserved.py
@@ -14,18 +14,18 @@ while len(reserved) != 0:
     stock_id = reserved[0]
     web.get('http://www.cmoney.tw/finance/f00025.aspx?s=' + str(stock_id))
     web.find_element_by_link_text('K線').click()
-    frames = web.find_elements_by_tag_name('iframe')
-    for frame_ind in range(len(frames)):
-        try:
-            web.switch_to_frame(frames[frame_ind])
-            btns = [btn for btn in web.find_elements_by_tag_name('input')
-                    if btn.get_attribute('value') == '周線']
-            if len(btns) != 0:
-                btns[0].click()
-                break
-        except selenium.common.exceptions.StaleElementReferenceException as err:
-            pass
-    web.switch_to_default_content()
+##    frames = web.find_elements_by_tag_name('iframe')
+##    for frame_ind in range(len(frames)):
+##        try:
+##            web.switch_to_frame(frames[frame_ind])
+##            btns = [btn for btn in web.find_elements_by_tag_name('input')
+##                    if btn.get_attribute('value') == '周線']
+##            if len(btns) != 0:
+##                btns[0].click()
+##                break
+##        except selenium.common.exceptions.StaleElementReferenceException as err:
+##            pass
+##    web.switch_to_default_content()
     web.execute_script("window.scrollTo(0,250);");
     web.get_screenshot_as_file('reserved/%04d.png' % (stock_id))
     del reserved[0]
--- read_stocks.py
+++ read_stocks.py
@@ -1,4 +1,4 @@
-import selenium
+import selenium, time
 from selenium import webdriver
 
 with open('stocks.txt', 'r') as file:
@@ -10,30 +10,33 @@ stocks.sort()
 stocks = list(set(stocks).difference(reserved))
 
 web = webdriver.Chrome()
+##web.get('http://www.cmoney.tw/finance/f00025.aspx?s=' + str(3034))
+##time.sleep(5)
 
 while len(stocks) != 0:
     print(len(stocks))
     stock_id = stocks[0]
     web.get('http://www.cmoney.tw/finance/f00025.aspx?s=' + str(stock_id))
     web.find_element_by_link_text('K線').click()
-    frames = web.find_elements_by_tag_name('iframe')
-    for frame_ind in range(len(frames)):
-        try:
-            web.switch_to_frame(frames[frame_ind])
-            btns = [btn for btn in web.find_elements_by_tag_name('input')
-                    if btn.get_attribute('value') == '周線']
-            if len(btns) != 0:
-                btns[0].click()
-                break
-        except selenium.common.exceptions.StaleElementReferenceException as err:
-            pass
-    web.switch_to_default_content()
-    web.execute_script("window.scrollTo(0,250);");
+##    frames = web.find_elements_by_tag_name('iframe')
+##    for frame_ind in range(len(frames)):
+##        try:
+##            web.switch_to_frame(frames[frame_ind])
+##            btns = [btn for btn in web.find_elements_by_tag_name('input')
+##                    if btn.get_attribute('value') == '周線']
+##            if len(btns) != 0:
+##                btns[0].click()
+##                break
+##        except selenium.common.exceptions.StaleElementReferenceException as err:
+##            pass
+##    web.switch_to_default_content()
+    web.execute_script("window.scrollTo(0,250);"); time.sleep(5)
     web.get_screenshot_as_file('stocks/%04d.png' % (stock_id))
     del stocks[0]
 
 web.quit()
 
-with open('stocks.txt', 'w') as file:
-    file.write('\n'.join([str(id) for id in stocks]))
+##with open('stocks.txt', 'w') as file:
+##    file.write('\n'.join([str(id) for id in stocks]))
 
+# vim: expandtab

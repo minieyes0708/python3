cd462cde58c9e4ef84c469c0f2fedff656810ce6
minieyes <chenvey2@gmail.com>
2020-07-19 23:08:58 +0800

    完成大改

--- eyny/eyny.py
+++ eyny/eyny.py
@@ -1,19 +1,30 @@
 class eyny:
     def __init__(self):
-        import time, selenium
-        while True:
-            try:
-                self.web = selenium.webdriver.Chrome()
-                self.web.get("http://www05.eyny.com/index.php")
-                self.web.find_element_by_link_text("登錄").click()
-                while not self.web.find_element_by_name("username").is_displayed(): time.sleep(1)
-                self.web.find_element_by_name("username").send_keys('chenvey2')
-                self.web.find_element_by_name("password").send_keys('shenfen520')
-                self.web.find_element_by_name("loginsubmit").click()
-            except:
-                raise
+        import dbm, time, selenium
+        self.db = dbm.open("./minieyes/db/eyny", "c")
+        self.web = selenium.webdriver.Chrome()
+        self.web.get("http://www05.eyny.com/index.php")
+        self.web.find_element_by_link_text("登錄").click()
+        while not self.web.find_element_by_name("username").is_displayed(): time.sleep(1)
+        self.web.find_element_by_name("username").send_keys('chenvey2')
+        self.web.find_element_by_name("password").send_keys('shenfen520')
+        self.web.find_element_by_name("loginsubmit").click()
     def __del__(self):
         self.web.quit()
+        self.db.close()
+    def insert_or_ignore(self, title, link):
+        if title not in self.db:
+            print("New Thread: " + title)
+            self.db[title] = link
+    def loop_and_remove(self):
+        keys = self.db.keys()
+        for i in range(len(keys)):
+            key = keys[i]
+            val = self.db[key].decode('utf8')
+            yield (i, key, val)
+            del self.db[key]
+    def count(self):
+        return len(self.db.keys())
     def waitfor(self, attrname, *args):
         import time, selenium
         while True:
@@ -32,3 +43,37 @@ class eyny:
             self.web.get("http://www05.eyny.com/forum-576-1.html")
             self.waitfor('get_element_by_name','submit').click()
             self.waitfor(self.web,'link_text','日韓電影(上傳空間)').click()
+        else:
+            self.web.get(where)
+    def confirm18(self):
+        import time
+        submit = [
+            tag
+            for tag in self.waitfor('find_elements_by_tag_name', 'input')
+            if '18歲' in tag.get_attribute('value')
+        ]
+        if len(submit):
+            submit[0].click()
+            time.sleep(5)
+    def get_mega_links(self):
+        # check url
+        def is_valid_url(url):
+            if url.startswidth('https://mega.nz'): return True
+            if url.startswidth('https://katfile.com'): return True
+            if url.startswidth('https://drives.google'): return True
+            return False
+        return [
+            val.get_attribute('href')
+            for val in self.web.find_elements_by_tag_name('a')
+            if val.get_attribute('href') != None and is_valid_url(val.get_attribute('href'))
+        ] + [
+            val.text
+            for val in self.web.find_elements_by_tag_name('td')
+            if 'https://' in val.text and is_valid_url(val.text[val.text.index('https://'):])
+        ]
+    def get_passwords(self):
+        return [
+            val.text
+            for val in self.web.find_elements_by_tag_name('td')
+            if '解壓密碼' in val.text
+        ]
--- eyny/eyny_update2.py
+++ eyny/eyny_update2.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
-
-import dbm
 from minieyes.eyny.eyny import eyny
 
-web = eyny()
-db = dbm.open("eyny", "c")
-
+eyny = eyny()
 for page in range(0, 20):
     print('page ' + str(page))
+
     # threads
     threads = [
         thread
-        for thread in web.waitfor('find_elements_by_tag_name', 'tbody')
+        for thread in eyny.waitfor('find_elements_by_tag_name', 'tbody')
         if thread.get_attribute("id").startswith('normalthread')
     ]
 
     # insert database
     for thread in threads:
-        link = thread.find_element_by_tag_name("th").find_element_by_class_name("xst").get_attribute("href")
-        title = thread.find_element_by_tag_name("th").find_element_by_class_name("xst").get_attribute("innerHTML")
-        if title not in db:
-            print("New Thread: " + title)
-            db[title] = title + '|||' + link
+        th = thread.find_element_by_tag_name("th")
+        xst = th.find_element_by_class_name("xst")
+        link = xst.get_attribute("href")
+        title = xst.get_attribute("innerHTML")
+        eyny.insert_or_ignore(title, link)
 
     # next page
-    web.waitfor('find_element_by_link_text','下一頁').click()
-
-db.close()
+    eyny.waitfor('find_element_by_link_text','下一頁').click()

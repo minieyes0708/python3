f84c06bba2805dd96b98cffa8717db15e7f0f0f3
minieyes <chenvey2@gmail.com>
2020-09-03 21:42:33 +0800

    download both category at once

--- eyny/eyny_update2.py
+++ eyny/eyny_update2.py
@@ -2,6 +2,7 @@
 from minieyes.eyny.eyny import eyny
 
 eyny = eyny()
+
 eyny.goto('本土電影')
 for page in range(0, 20):
     print('page ' + str(page))
@@ -22,4 +23,26 @@ for page in range(0, 20):
         eyny.insert_or_ignore(title, link)
 
     # next page
-    eyny.waitfor('find_element_by_link_text','下一頁').click()
+    eyny.waitfor('find_element_by_link_text', '下一頁').click()
+
+eyny.goto('日韓電影')
+for page in range(0, 20):
+    print('page ' + str(page))
+
+    # threads
+    threads = [
+        thread
+        for thread in eyny.waitfor('find_elements_by_tag_name', 'tbody')
+        if thread.get_attribute("id").startswith('normalthread')
+    ]
+
+    # insert database
+    for thread in threads:
+        th = thread.find_element_by_tag_name("th")
+        xst = th.find_element_by_class_name("xst")
+        link = xst.get_attribute("href")
+        title = xst.get_attribute("innerHTML")
+        eyny.insert_or_ignore(title, link)
+
+    # next page
+    eyny.waitfor('find_element_by_link_text', '下一頁').click()

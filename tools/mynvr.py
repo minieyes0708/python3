ee690e511a75cac7af58c02c72656e1d9269e80e
minieyes <chenvey2@gmail.com>
2021-08-31 05:30:12 +0800

    concat all args with space

--- tools/mynvr.py
+++ tools/mynvr.py
@@ -5,5 +5,5 @@ if len(sys.argv) < 2: os.exit(0)
 pipes = [v for v in os.listdir(r'\\.\pipe') if v.startswith('nvim')]
 if len(pipes) == 0: os.exit(1)
 
-cmd = r'nvr --servername \\.\pipe\\' + pipes[0] + ' ' + sys.argv[1]
-print(cmd); os.system(cmd);
+cmd = r'nvr --servername \\.\pipe\\' + pipes[0] + ' ' + ' '.join(sys.argv[1:])
+os.system(cmd)

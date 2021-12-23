5575e9dd849fc18a1d3881424de4e87491af4b52
minieyes <chenvey2@gmail.com>
2021-12-23 22:48:04 +0800

    mynvr.py simply print out server name

--- tools/mynvr.py
+++ tools/mynvr.py
@@ -1,9 +1,10 @@
 import os, sys
 
-if len(sys.argv) < 2: os.exit(0)
+pipes = [
+    v for v in os.listdir(r'\\.\pipe')
+    if v.startswith('nvim') and not v.startswith('nvim-term-out')]
+if len(pipes) == 0:
+    print("Cannot Find nvim Server", file = sys.stderr)
+    sys.exit(1) 
 
-pipes = [v for v in os.listdir(r'\\.\pipe') if v.startswith('nvim')]
-if len(pipes) == 0: os.exit(1)
-
-cmd = r'nvr --servername \\.\pipe\\' + pipes[0] + ' ' + ' '.join(sys.argv[1:])
-os.system(cmd)
+print(r'\\.\pipe\\' + pipes[0])

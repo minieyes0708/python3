bfdb2bbe7568e10c866e7196a5ceaef456314260
Minieyes Chen <chenvey2@gmail.com>
2024-07-21 07:03:55 +0800

    update eyny_choose to use new selenium api

--- eyny/eyny_choose.py
+++ eyny/eyny_choose.py
@@ -1,34 +1,33 @@
 from selenium import webdriver
-import dbm, re, os, time, codecs, urllib.request
-
-if __name__ == 'minieyes.eyny_choose':
-    PATH = 'minieyes/'
-else:
-    PATH = ''
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import dbm, re, os, time, codecs, urllib.request, getpass
     
 #########
 # login #
 #########
 web = webdriver.Chrome()
 web.get("http://www05.eyny.com/index.php")
-web.find_element_by_link_text("登錄").click()
-while not web.find_element_by_name("username").is_displayed():
+web.find_element(By.LINK_TEXT, "登錄").click()
+while not web.find_element(By.NAME, "username").is_displayed():
     time.sleep(1)
-web.find_element_by_name("username").send_keys('chenvey2')
-web.find_element_by_name("password").send_keys('shenfen520')
-web.find_element_by_name("loginsubmit").click()
+web.find_element(By.NAME, "username").send_keys('chenvey2')
+web.find_element(By.NAME, "password").send_keys(getpass.getpass())
+Select(web.find_element(By.NAME, 'questionid')).select_by_index(1)
+web.find_element(By.NAME, "answer").send_keys(getpass.getpass('母親名字: '))
+web.find_element(By.NAME, "loginsubmit").click()
 
 def translate(str):
     web.get('https://translate.google.com.tw/#ja/zh-TW/' +
             urllib.parse.urlencode({'p':str})[2:])
-    while not len(web.find_element_by_id('result_box').get_attribute('innerHTML')): pass
-    return web.find_element_by_id('result_box').get_attribute('innerHTML')
+    while not len(web.find_element(By.ID, 'result_box').get_attribute('innerHTML')): pass
+    return web.find_element(By.ID, 'result_box').get_attribute('innerHTML')
 
 ############
 # database #
 ############
-newdb = dbm.open(PATH+'eyny_new', 'c')
-tododb = dbm.open(PATH+'eyny_todo', 'c')
+newdb = dbm.open('./eyny/db/eyny_new', 'c')
+tododb = dbm.open('./eyny/db/eyny_todo', 'c')
 
 try:
     keys = list(newdb.keys())
@@ -55,13 +54,13 @@ try:
                 if picture[0:4] == 'http':
                     req = urllib.request.Request(picture);
                     req.add_header('Referer', 'http://www05.eyny.com/index.php')
-                    with open(PATH+'test.jpg','wb') as file:
+                    with open('./eyny/db/test.jpg','wb') as file:
                         file.write(urllib.request.urlopen(req).read())
-                with codecs.open(PATH+'eyny.html', 'w', 'utf8') as file:
+                with codecs.open('./eyny/db/eyny.html', 'w', 'utf8') as file:
                     file.write(r"<meta http-equiv='Content-Type' content='text/html; charset=utf8'/>")
                     file.write('({0}/{1})<img src="test.jpg"/><br/>{2}<br/>'.format(index+1, len(keys), title))
                     # file.write(translate(title))
-                web.get('file:///' + os.path.join(os.getcwd(), PATH, 'eyny.html'))
+                web.get('file:///' + os.path.join(os.getcwd(), './eyny/db/eyny.html'))
                 web.refresh()
             else:
                 print('no more data')
@@ -84,7 +83,7 @@ try:
         if cmd[0] == 'l':
             web.get(link)
             ##### 滿18歲 #####
-            submit = [tag for tag in web.find_elements_by_tag_name('input') if re.search('18歲',tag.get_attribute('value'))]
+            submit = [tag for tag in web.find_elements(By.TAG_NAME, 'input') if re.search('18歲',tag.get_attribute('value'))]
             if len(submit): submit[0].click()
         if len(cmd) == 1: cmd = input('$ ')
         else: cmd = cmd[1:]
